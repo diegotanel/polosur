958f517ab480918e775a97d83d9b0eebd8997463
Diego Tanel <diego.tanel@tecneg.com>
2013-07-11 13:03:23 -0300

    #1066 comment: se dejaron solo las primera cuatro columnas en la vista de lista de clientes state: done

--- spec/views/clientes/index.html.erb_spec.rb
+++ spec/views/clientes/index.html.erb_spec.rb
@@ -7,35 +7,13 @@ describe "clientes/index" do
         :nombre_comercial => "Nombre Comercial",
         :razon_social => "Razon Social",
         :codigo_interno => "Codigo Interno",
-        :cuit => "Cuit",
-        :condicion_de_iva => "Condicion De Iva",
-        :telefono => "Telefono",
-        :direccion => "Direccion",
-        :localidad => "Localidad",
-        :ciudad => "Ciudad",
-        :provincia => "Provincia",
-        :codigo_postal => "Codigo Postal",
-        :email => "Email",
-        :contacto => "Contacto",
-        :lugar_de_entrega => "Lugar De Entrega",
-        :observaciones => "MyText"
+        :cuit => "Cuit"
       ),
       stub_model(Cliente,
         :nombre_comercial => "Nombre Comercial",
         :razon_social => "Razon Social",
         :codigo_interno => "Codigo Interno",
-        :cuit => "Cuit",
-        :condicion_de_iva => "Condicion De Iva",
-        :telefono => "Telefono",
-        :direccion => "Direccion",
-        :localidad => "Localidad",
-        :ciudad => "Ciudad",
-        :provincia => "Provincia",
-        :codigo_postal => "Codigo Postal",
-        :email => "Email",
-        :contacto => "Contacto",
-        :lugar_de_entrega => "Lugar De Entrega",
-        :observaciones => "MyText"
+        :cuit => "Cuit"
       )
     ])
   end
@@ -47,16 +25,5 @@ describe "clientes/index" do
     assert_select "tr>td", :text => "Razon Social".to_s, :count => 2
     assert_select "tr>td", :text => "Codigo Interno".to_s, :count => 2
     assert_select "tr>td", :text => "Cuit".to_s, :count => 2
-    assert_select "tr>td", :text => "Condicion De Iva".to_s, :count => 2
-    assert_select "tr>td", :text => "Telefono".to_s, :count => 2
-    assert_select "tr>td", :text => "Direccion".to_s, :count => 2
-    assert_select "tr>td", :text => "Localidad".to_s, :count => 2
-    assert_select "tr>td", :text => "Ciudad".to_s, :count => 2
-    assert_select "tr>td", :text => "Provincia".to_s, :count => 2
-    assert_select "tr>td", :text => "Codigo Postal".to_s, :count => 2
-    assert_select "tr>td", :text => "Email".to_s, :count => 2
-    assert_select "tr>td", :text => "Contacto".to_s, :count => 2
-    assert_select "tr>td", :text => "Lugar De Entrega".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
   end
 end

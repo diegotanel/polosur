d1fdd7bae9dafa9fef0a2978d678067fd7b725f4
Diego Tanel <diego.tanel@tecneg.com>
2013-07-12 00:33:06 -0300

    se corrigio el test articulos_controller agregando el tipo de envase a valid_attributes

--- spec/controllers/articulos_controller_spec.rb
+++ spec/controllers/articulos_controller_spec.rb
@@ -23,7 +23,7 @@ describe ArticulosController do
   # This should return the minimal set of attributes required to create a valid
   # Articulo. As you add validations to Articulo, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "codigo" => "MyString" } }
+  let(:valid_attributes) { { "codigo" => "MyString", :tipo_de_envase_id => 1 } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
